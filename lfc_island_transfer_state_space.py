6d5a2286ad129bd64d100f4caeb0b81dbad82be0
Siazis <andrew.d.syrmakesis@gmail.com>
2022-02-24 01:18:32 +0200

    attack matrix integration

--- lfc_island_transfer_state_space.py
+++ lfc_island_transfer_state_space.py
@@ -22,8 +22,8 @@ A = [[-D/M, 1/M, 0, 0],
 	]
 
 B = [[-1/M],
-	 [0,],
-	 [0,],
+	 [0],
+	 [0],
 	 [0]
 	]
 
@@ -31,11 +31,44 @@ C = [[1, 0, 0, 0],
 	 [0, 0, 0, 1.0],
 	]
 
-island_lfc = ss(A, B, C, 0)
+# Attack matrix
+F = [[0],
+	 [0],
+	 [0],
+	 [1]
+	]
 
-response = step_response(island_lfc)
+manual = 1
+attack = 1
 
-plt.plot(response.time, response.outputs[0][0])
+if attack == 0:
+	island_lfc = ss(A, B, C, 0)
+
+	if manual == 0:
+		response = step_response(island_lfc)
+		plt.plot(response.time, response.outputs[0][0])
+	elif manual == 1:
+		T = np.arange(0, 30, 0.1, dtype=float).tolist()
+		U = [np.ones_like(T)]
+
+		t, y = forced_response(island_lfc, T=T, U=U)
+		plt.plot(t, y[0])
+elif attack == 1:
+	BF = np.hstack([B, F]).tolist()
+	island_lfc = ss(A, BF, C, 0)
+
+	if manual == 0:
+		response = step_response(island_lfc)
+		plt.plot(response.time, response.outputs[0][0])
+	elif manual == 1:
+		attack_time = 100
+
+		T = np.arange(0, 30, 0.1, dtype=float).tolist()
+		attack_array = np.append(np.zeros_like(T[:attack_time]), np.sin(T[attack_time:]))
+		U = [np.zeros_like(T), attack_array]
+
+		t, y = forced_response(island_lfc, T=T, U=U)
+		plt.plot(t, y[0])
+
 plt.grid()
-
 plt.show()
