0e8a4c1912a5a45060b6c556ab861db9347b891c
Siazis <andrew.d.syrmakesis@gmail.com>
2022-02-24 14:28:19 +0200

    transformation matrices introduced [not correct]

--- lfc_island_transfer_state_space.py
+++ lfc_island_transfer_state_space.py
@@ -1,10 +1,13 @@
 from control import *
+from numpy.linalg import inv, solve
 import matplotlib.pyplot as plt
+
+np.set_printoptions(precision=3, suppress=True)
 
 # In this script, the Load Frequency Control system of a single
 # area is defined by its state-space representation
 
-# Parameters
+# parameters
 D, M = 1, 10
 Tg = 0.1
 Tt = 0.3
@@ -14,14 +17,14 @@ beta = 20
 # integral controller gain
 K = 0.9
 
-# State, input, output matrices
-A = [[-D/M, 1/M, 0, 0],
+# state, input, output matrices
+AB = [[-D/M, 1/M, 0, 0],
 	 [0, -1/Tt, 1/Tt, 0],
 	 [-1/(R*Tg), 0, -1/Tg, -K/Tg],
 	 [beta, 0, 0, 0]
 	]
 
-B = [[-1/M],
+E = [[-1/M],
 	 [0],
 	 [0],
 	 [0]
@@ -31,31 +34,61 @@ C = [[1, 0, 0, 0],
 	 [0, 0, 0, 1.0],
 	]
 
-# Attack matrix
+# attack matrix
 F = [[0],
 	 [0],
 	 [0],
 	 [1]
 	]
 
+# transformation matrices
+
+# T is selected so that
+# 		[1]
+# TxE =	[0]
+#		[0]
+#		[0]
+
+T = [[-10, 0, 0, -10],
+	 [0, 3, 0.4, -6.3],
+	 [0, 7.1, 1.4, 9.5],
+	 [0, -4.3, 6.8, 4.2]
+	]
+T_inv = inv(T)
+
+S = [[1, 1],
+	 [0, 0],
+	]
+
+# calculate the system matrices under the new coordinate
+
+A_t = np.dot(np.dot(T, AB), T_inv)
+B_t = 1
+E_t = np.dot(T, E)
+C_t = np.dot(np.dot(S, C), T_inv)
+
+print(A_t)
+print(E_t)
+print(C_t)
+
 manual = 1
 attack = 1
 
 if attack == 0:
-	island_lfc = ss(A, B, C, 0)
+	island_lfc = ss(AB, E, C, 0)
 
 	if manual == 0:
 		response = step_response(island_lfc)
 		plt.plot(response.time, response.outputs[0][0])
 	elif manual == 1:
-		T = np.arange(0, 30, 0.1, dtype=float).tolist()
+		T = np.arange(0, 30, 0.1, dtype=float)
 		U = [np.ones_like(T)]
 
 		t, y = forced_response(island_lfc, T=T, U=U)
 		plt.plot(t, y[0])
 elif attack == 1:
-	BF = np.hstack([B, F]).tolist()
-	island_lfc = ss(A, BF, C, 0)
+	EF = np.hstack([E, F]).tolist()
+	island_lfc = ss(AB, EF, C, 0)
 
 	if manual == 0:
 		response = step_response(island_lfc)
@@ -63,7 +96,7 @@ elif attack == 1:
 	elif manual == 1:
 		attack_time = 100
 
-		T = np.arange(0, 30, 0.1, dtype=float).tolist()
+		T = np.arange(0, 30, 0.1, dtype=float)
 		attack_array = np.append(np.zeros_like(T[:attack_time]), np.sin(T[attack_time:]))
 		U = [np.zeros_like(T), attack_array]
 
@@ -71,4 +104,4 @@ elif attack == 1:
 		plt.plot(t, y[0])
 
 plt.grid()
-plt.show()
+# plt.show()
